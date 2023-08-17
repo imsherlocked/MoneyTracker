3c59bbddbfa9175257c5acd09580368cb5f633d3
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-16 23:24:08 -0400

    added a html structure for the page

--- money-tracker/src/App.js
+++ money-tracker/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        $400 <span>.00</span>
+      </h1>
+      <form>
+        <div className="basics">
+          <input type="text" placeholder={"+200 new Samsung Tv"}></input>
+          <input type="datetime-local"></input>
+        </div>
+        <div className="decription">
+          <input type="text" placeholder={"decription"}></input>
+        </div>
+      </form>
+    </main>
   );
 }
 
