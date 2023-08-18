aae13f1fa612d29ed249841b47769983e26ac4ac
Shivam Nahar <shivamnahar09@gmail.com>
2023-08-17 20:47:08 -0400

    fixes for POST api

--- money-tracker/api/script.js
+++ money-tracker/api/script.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.get("/api/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post("api/transaction", (req, res) => {
+app.post("/api/transaction", (req, res) => {
   res.json(req.body);
 });
 
